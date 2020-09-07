3d7729b83007d9325b4d0c7a8405bc53417a1434
Fabian Higuita <fabianes1996@gmail.com>
2020-09-07 17:43:18 -0500

    Add card

--- js/main.js
+++ js/main.js
@@ -14,6 +14,7 @@ buttonLeft.addEventListener('click', function(){
 buttonRight.addEventListener('click', function(){
     document.getElementById("gifs").scrollLeft += 500;
 })
+
 async function callGifs() {
   try {
     let call = await fetch(URL + API_KEY + LIMIT);
@@ -27,16 +28,67 @@ async function callGifs() {
   // div.setAttribute("src", "https://media3.giphy.com/media/Y1Xxlg8verQGdFQyCz/200.gif?cid=cfc0aab7abfdd810b3e44d4fba37c2187afb2283c70f7f4b&rid=200.gif");
   // container.appendChild(div)
 }
-function addGifs(){
+function addGifs(){  
     for (let i = 0; i < gifs.length; i++) {        
-        let image = document.createElement("img");        
-        image.setAttribute("src", `${gifs[i].images.original.url}`);
-        image.style.width = "357px";
-        image.style.height = "275px";
-        image.style.marginRight = "29px"
-        container.appendChild(image);        
-    }
+        let square = document.createElement('div'); 
+        square.setAttribute('class', 'square');    
+
+        let card = document.createElement('div');
+        card.setAttribute('class', 'card-icons')      
+
+        let divIconFav = document.createElement('div');       
+        divIconFav.setAttribute('class', 'div-icons');
+        let image1 = document.createElement('img')
+        image1.setAttribute('src', 'assets/icon-fav-hover.svg'); 
+        image1.setAttribute('class', 'icono');       
+        divIconFav.appendChild(image1);
+        card.appendChild(divIconFav);
+
+        let divIconDownload = document.createElement('div');       
+        divIconDownload.setAttribute('class', 'div-icons');
+        let image2 = document.createElement('img')
+        image2.setAttribute('src', 'assets/icon-download.svg');
+        image2.setAttribute('class', 'icono');
+        divIconDownload.appendChild(image2);
+        card.appendChild(divIconDownload);
+
+        let divIconMax = document.createElement('div');       
+        divIconMax.setAttribute('class', 'div-icons');
+        let image3 = document.createElement('img')
+        image3.setAttribute('src', 'assets/icon-max.svg');
+        image3.setAttribute('class', 'icono');
+        divIconMax.appendChild(image3);   
+        card.appendChild(divIconMax);
+        
+        square.appendChild(card)
+
+        let gif = document.createElement("img");
+        gif.setAttribute("src", `${gifs[i].images.original.url}`);
+        gif.setAttribute("class", 'gifos');
+        gif.style.width = "357px";
+        gif.style.height = "275px"  ;      
+        square.appendChild(gif);
+        let divParagraphs = document.createElement('div');
+        divParagraphs.setAttribute("class", 'div-description');
+        let p1 = document.createElement('p');
+        p1.textContent = `${gifs[i].username}`;
+        let p2 = document.createElement("p");
+        p2.textContent = `${gifs[i].title}`;
+        divParagraphs.appendChild(p1);
+        divParagraphs.appendChild(p2);
+        square.appendChild(divParagraphs);
+        container.appendChild(square);
+        
+        // let image = document.createElement("img");                
+        // image.setAttribute("src", `${gifs[i].images.original.url}`);
+        // image.style.width = "357px";
+        // image.style.height = "275px";
+        // image.style.marginRight = "29px";            
+        // div.appendChild(image);                
+        
+    }      
 }
+
 
 function addImage() {
   let image = document.createElement("img");
