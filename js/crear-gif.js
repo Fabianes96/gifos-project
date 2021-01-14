ae449b620e542e89c36462b3f2da938f357f94a8
Fabian Higuita <fabianes1996@gmail.com>
2021-01-13 22:07:36 -0500

    Update

--- js/crear-gif.js
+++ js/crear-gif.js
@@ -1,15 +1,42 @@
 import * as global from "./global.js";
+let recorder = RecordRTC(stream, {
+    type: 'gif',
+    frameRate: 1,
+    quality: 10,
+    width: 360,
+    hidden: 240,
+    onGifRecordingStarted: function() {
+     console.log('started')
+   },
+});
 
-global.btnComenzar.addEventListener("click",async()=>{
+global.btnComenzar.addEventListener("click",async()=>{    
     try {        
-        global.cameraWindow.style.display = 'none'
+        global.btnComenzar.classList.remove('btn-mas')
+        global.btnComenzar.classList.add('none')
+        global.cameraWindow.classList.remove('camera-window-content');
+        global.cameraWindow.classList.add('none');
+        global.cameraWindow2.classList.remove('none');
+        global.cameraWindow2.classList.add('camera-window.content');
+        await activeCamera()         
+    } catch (error) {
+        console.log(error);
+    }
+    
+})
+
+async function activeCamera(){
+    try {                
         global.video.classList.remove('none');
         const stream = await navigator.mediaDevices.getUserMedia({
             audio: false,
             video:true
         })
         global.video.srcObject = stream;
+        global.video.play()
+        global.btnGrabar.classList.remove('none');
+        global.btnGrabar.classList.add('btn-mas');
     } catch (error) {
         console.log(error);
     }
-})
+}
--- js/global.js
+++ js/global.js
@@ -29,7 +29,9 @@ let btnMasFavs = document.getElementById("btn-mas-favs");
 let ulSuggestions = document.getElementById("suggestions");   
 let links = document.getElementsByClassName("links");
 let btnComenzar =document.getElementById("btn-comenzar")
+let btnGrabar =document.getElementById("btn-grabar")
 let cameraWindow = document.getElementById("camera-window");
+let cameraWindow2 = document.getElementById("camera-window-2");
 let video = document.querySelector('video');
 
-export {initial,misGIFOS,API_KEY,LIMIT,URL,btnCloseSearch,btnMas,btnMasFavs,buttonLeft,buttonLeftModal,buttonRight,buttonRightModal,central,container,containerFavorites,containerGIFOS,containerSearchResults,divGifs,favoritos,linkFavoritos, linkGIFOS,modal, modalContent,noFavorites,noResults,searchContainer, searchField,span, ulSuggestions, links, btnComenzar, cameraWindow, video}
+export {initial,misGIFOS,API_KEY,LIMIT,URL,btnCloseSearch,btnMas,btnMasFavs,buttonLeft,buttonLeftModal,buttonRight,buttonRightModal,central,container,containerFavorites,containerGIFOS,containerSearchResults,divGifs,favoritos,linkFavoritos, linkGIFOS,modal, modalContent,noFavorites,noResults,searchContainer, searchField,span, ulSuggestions, links, btnComenzar,btnGrabar, cameraWindow, cameraWindow2, video}
